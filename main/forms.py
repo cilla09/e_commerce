26a5e369bcfe0aad5ef964e8d1b1187b30377edc
cilla09 <priscilla.natanael@ui.ac.id>
2024-10-07 11:00:49 +0700

    Tugas 6: debugging error validation

--- main/forms.py
+++ main/forms.py
@@ -1,28 +1,38 @@
-from django.forms import ModelForm
+from django import forms
 from main.models import Product
 from django.utils.html import strip_tags
 
-class ProductForm(ModelForm):
+class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ["name", "price", "description", "stock", "category"]
 
-        def clean_name(self):
-            name = self.cleaned_data["name"]
-            return strip_tags(name)
+    def clean_name(self):
+        name = strip_tags(self.cleaned_data.get('name', ''))
+        if not name.strip():
+            raise forms.ValidationError('This field cannot be blank.')
+        return name
 
-        def clean_price(self):
-            price = self.cleaned_data["price"]
-            return strip_tags(price)
-        
-        def clean_description(self):
-            description = self.cleaned_data["description"]
-            return strip_tags(description)
+    def clean_description(self):
+        description = strip_tags(self.cleaned_data.get('description', ''))
+        if not description.strip():
+            raise forms.ValidationError('This field cannot be blank.')
+        return description
 
-        def clean_price(self):
-            stock = self.cleaned_data["stock"]
-            return strip_tags(stock)
-        
-        def clean_category(self):
-            category = self.cleaned_data["category"]
-            return strip_tags(category)
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if not price:
+            raise forms.ValidationError('This field cannot be blank.')
+        return price
+
+    def clean_stock(self):
+        stock = self.cleaned_data.get('stock')
+        if not stock:
+            raise forms.ValidationError('This field cannot be blank.')
+        return stock
+
+    def clean_category(self):
+        category = strip_tags(self.cleaned_data.get('category', ''))
+        if not category.strip():
+            raise forms.ValidationError('This field cannot be blank.')
+        return category
